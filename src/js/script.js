350ee7673dd4e07c611e0b36b8f391d28340e2ee
Mikolaj Renke <mikolaj.renke@gmail.com>
2022-03-05 23:52:53 +0100

    Add JS class CartProduct

--- src/js/script.js
+++ src/js/script.js
@@ -399,7 +399,51 @@
 
       menuContainer.appendChild(thisCart.element);
 
-      console.log('adding Cart Product', menuProduct, thisCart);
+      thisCart.products.push(menuProduct);
+
+      console.log('thisCart.products', thisCart.products);
+    }
+  }
+
+  class CartProduct {
+    constructor(menuProduct, element) {
+      const thisCartProduct = this;
+
+      thisCartProduct.id = menuProduct.id;
+      thisCartProduct.name = menuProduct.name;
+      thisCartProduct.amount = menuProduct.amount;
+      thisCartProduct.priceSinlge = menuProduct.priceSingle;
+      thisCartProduct.price = menuProduct.price;
+      thisCartProduct.params = menuProduct.params;
+
+      thisCartProduct.getElements(element);
+
+      console.log('thisCartProduct', thisCartProduct);
+    }
+
+    getElements(element) {
+      const thisCartProduct = this;
+
+      thisCartProduct.dom = {};
+
+      thisCartProduct.dom.wrapper = element;
+
+      thisCartProduct.dom.amountWidget =
+        thisCartProduct.dom.wrapper.querySelector(
+          select.cartProduct.amountWidget
+        );
+
+      thisCartProduct.dom.price = thisCartProduct.dom.wrapper.querySelector(
+        select.cartProduct.price
+      );
+
+      thisCartProduct.dom.edit = thisCartProduct.dom.wrapper.querySelector(
+        select.cartProduct.edit
+      );
+
+      thisCartProduct.dom.remove = thisCartProduct.dom.wrapper.querySelector(
+        select.cart.remove
+      );
     }
   }
 
