343f3ba31fbba2cf813f890e4c1d355c8f8a48b2
Mikolaj Renke <mikolaj.renke@gmail.com>
2022-03-17 22:29:09 +0100

    Add JS initPages() + activatePage()

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,54 @@ import Product from './components/Product.js';
 import Cart from './components/Cart.js';
 
 const app = {
+  initPages: function () {
+    const thisApp = this;
+
+    thisApp.pages = document.querySelector(select.containerOf.pages).children;
+    thisApp.navLinks = document.querySelectorAll(select.nav.links);
+
+    const idFromHash = window.location.hash.replace('#/', '');
+
+    let pageMatchingHash = thisApp.pages[0].id;
+
+    for (let page of thisApp.pages) {
+      if (page.id == idFromHash) {
+        pageMatchingHash = page.id;
+        break;
+      }
+    }
+
+    thisApp.activatePage(pageMatchingHash);
+
+    for (let link of thisApp.navLinks) {
+      link.addEventListener('click', function (event) {
+        const clickedElement = this;
+        event.preventDefault();
+
+        const id = clickedElement.getAttribute('href').replace('#', '');
+
+        thisApp.activatePage(id);
+
+        window.location.hash = '#/' + id;
+      });
+    }
+  },
+
+  activatePage: function (pageId) {
+    const thisApp = this;
+
+    for (let page of thisApp.pages) {
+      page.classList.toggle(classNames.pages.active, page.id == pageId);
+    }
+
+    for (let link of thisApp.navLinks) {
+      link.classList.toggle(
+        classNames.nav.active,
+        link.getAttribute('href') == '#' + pageId
+      );
+    }
+  },
+
   initMenu: function () {
     const thisApp = this;
 
@@ -45,6 +93,8 @@ const app = {
     console.log('settings:', settings);
     console.log('templates:', templates);
 
+    thisApp.initPages();
+
     thisApp.initData();
     thisApp.initCart();
   },
@@ -58,7 +108,7 @@ const app = {
     thisApp.productList = document.querySelector(select.containerOf.menu);
 
     thisApp.productList.addEventListener('add-to-cart', function (event) {
-      app.cart.add(event.detail.product);
+      app.cart.add(event.detail.productData);
     });
   },
 };
